3bb8d2e3bcd21b0d23992b5c8bfb9bac1faf9ff3
Jooyoung LEE <joolee0918@gmail.com>
2019-11-04 17:38:37 -0500

    sigma

--- R/ffcoxpenal.fit.R
+++ R/ffcoxpenal.fit.R
@@ -661,7 +661,7 @@ ffcoxpenal.fit <- function(x, y, strata, offset, init, control,
               Omega <- t(DD)%*%DD/2
 
               d2l <- solve(-coxLik$var)
-              df <- sum(diag( solve(d2l - Omega - Sigma)%*%d2l ) )
+              df <- sum(diag( solve(d2l - Omega)%*%d2l ) )
               loglik <- coxLik$loglik[1]
 
               gcv <- -coxLik$loglik[1]/(n*(1-df/n)^2)
