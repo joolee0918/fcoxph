f6f06c3baf5b8b1d33ceefe19e1ce4747364cfd0
Jooyoung LEE <joolee0918@gmail.com>
2020-04-06 18:13:25 -0400

    G

--- R/f0coxpenal.fit.R
+++ R/f0coxpenal.fit.R
@@ -387,12 +387,13 @@ fcoxpenal.fit <- function(x, y, strata, offset, init, control,
       thetalist[[i]] <- Theta[iter]
       lambdalist[[i]] <- 0
 
+      G <- sm[[1]]$S[[1]]*thetalist[[1]]/(1-thetalist[[1]])
+
       if(L2penalty == "none"){
         D[[i]] <- 0
         Dstar <- as.matrix(0)
         Dncol <- 0
         Dnrow <- 0
-        G <- as.matrix(0)
         nystar <- nvar
       } else{
         if(is.null(sm[[i]]$D)) {
@@ -405,7 +406,6 @@ fcoxpenal.fit <- function(x, y, strata, offset, init, control,
         Dncol <- sum(sapply(D, ncol))
         Dnrow <- sum(sapply(D, nrow))
         Dstar <- matrix(0, nrow=Dnrow, ncol=nvar)
-        G <- sm[[1]]$S[[1]]*thetalist[[1]]/(1-thetalist[[1]])
         nystar <- nvar + Dnrow
       }
     }
