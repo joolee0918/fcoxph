6288af6c73149e63944e557630105b5c251ce894
Jooyoung LEE <joolee0918@gmail.com>
2020-04-10 14:07:10 -0400

    fd

--- R/coef.foxph.R
+++ R/coef.foxph.R
@@ -1,23 +1,23 @@
 
 #' @importFrom mgcv PredictMat
+#' #' @importFrom fda fd
 #' @export
 coef.fcoxph <-  function (x,  n=100){
 
    m <- length(x$smooth)
    fit <- vector(mode = "list", length=m)
    for(i in 1:m){
-   raw <- x$fcoxph$fs[[i]]$xind
-   xx <- seq(min(raw), max(raw), length = n)
+   xx <- seq(x$smooth[[i]]$beta.basis$rangeval[1], x$smooth[[i]]$beta.basis$rangeval[2], length = n)
 
   first <- x$smooth[[i]]$first.para
   last <- x$smooth[[i]]$last.para
   p <- x$coefficients[first:last]
   beta.basis <- x$smooth[[i]]$beta.basis
   X <- fda::eval.basis(xx, beta.basis)
-
+  betafd <- fda::fd(coef=p,basisobj=beta.basis)
 
   fit[[i]]$s <- xx
-  fit[[i]]$value <- eval.fd(xx, p)
+  fit[[i]]$value <- eval.fd(xx, betafd)
   fit[[i]]$se <- sqrt(pmax(0, rowSums((X %*% x$var[first:last, first:last, drop = FALSE]) *
                                    X)))
   if(!is.null(x$naive.var))  fit[[i]]$naive.se <- sqrt(pmax(0, rowSums((X %*% x$naive.var[first:last, first:last, drop = FALSE]) *
