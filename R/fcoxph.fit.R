5a0d93c36196c64da053ce2fdf53de7c8c2f1ba8
joolee0918 <48692713+joolee0918@users.noreply.github.com>
2022-03-21 21:52:40 +0900

    Update fcoxph.fit.R

--- R/fcoxph.fit.R
+++ R/fcoxph.fit.R
@@ -379,14 +379,7 @@ fcoxph.fit <- function(formula, data, weights, subset, na.action,
     if(tuning.method %in% c("aic", "bic", "gcv", "cv")){
 
     fit <- list()
-
     
-     print(fit0$beta)
-        print(length(fit0$beta))
-      
-      print(fit0$beta[[fsel]])
-      print(dim(fit0$beta[[fsel]]))
-      
     fit$coefficients <- fit0$beta[[fsel]][, sel[fsel]]
     names(fit$coefficients) <- fit0$varnames
     fit$history <- fit0$beta
